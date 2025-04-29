1cea08be28638f4daea48ddcf643fb17ec08c422
Ymirko <Ymirko@DESKTOP-V3P4O0Q>
2025-04-29 15:22:47 +0200

    nuevo cambio

---
+++ src/inicio_erica/nombre.java
@@ -0,0 +1,7 @@
+package inicio_erica;
+
+public class nombre {
+    public static void main(String[] args) {
+        System.out.println("erica");
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module inicio_erica {
+}
