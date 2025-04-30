8df9db9cca4c67eb7f5ba9343f92283e7ae19086
Manuel <Manuel@DESKTOP-manu>
2025-04-30 12:07:23 +0200

    cambio de nombre y persona que edita

--- src/inicio_erica/nombre.java
+++ src/inicio_erica/nombre.java
@@ -2,7 +2,9 @@ package inicio_erica;
 
 public class nombre {
     public static void main(String[] args) {
-        System.out.println("erica");
-        System.out.println("diez");
+        System.out.println("ERICA MAYUSCULA");
+        System.out.println("DIEZ MAYUSCULA");
+        System.out.println("editado por manuu");
+        
     }
 }
